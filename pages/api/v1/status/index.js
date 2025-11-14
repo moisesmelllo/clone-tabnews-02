54401b59a359af7c4ece389658df8697cf132a16
moisesmelllo <moisesmvferreira@gmail.com>
2025-11-14 10:59:47 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const maxConnectionsQuery = await database.query("SHOW max_connections");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnectionsQuery = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const activeConnections = activeConnectionsQuery.rows[0].count;
+
+  const versionQuery = await database.query("SHOW server_version;");
+  const version = versionQuery.rows[0].server_version;
   response.status(200).json({
-    statusCode: response.statusCode,
-    url: response.req.url,
-    version: response.req.httpVersion,
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        active_connections: parseInt(activeConnections),
+        version: version,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,20 @@
 test("GET tp /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const path = responseBody.dependencies.database;
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(path.max_connections).toEqual(100);
+
+  expect(path.active_connections).toEqual(1);
+
+  expect(path.version).toEqual("16.0");
+
+  expect(responseBody).not.toHaveProperty("password");
+  expect(responseBody).not.toHaveProperty("email");
 });
