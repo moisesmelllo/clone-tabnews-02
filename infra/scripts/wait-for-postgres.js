3d0f7a0a323a1bb9c5922ba4dc8b5c2a23b15d0e
moisesmelllo <moisesmvferreira@gmail.com>
2025-11-20 12:47:00 -0300

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,20 @@
+const { exec } = require("node:child_process");
+
+function checkForPostgres() {
+  const result = exec(
+    "docker exec postgres-dev pg_isready --host localhost",
+    callBackFunction,
+  );
+
+  function callBackFunction(stderr, stdout) {
+    if (stdout.includes("accepting connections")) {
+      console.log("\n\n🟢 Banco esta pronto para receber conexoes");
+      return true;
+    }
+    process.stdout.write(".");
+    checkForPostgres();
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando conexoes ao banco");
+checkForPostgres();
