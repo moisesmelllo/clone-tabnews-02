c722040b217944e78190d2be6a823bbb6973eb50
moises <moisesmvferreira@gmail.com>
2026-03-27 12:09:02 -0300

    refactor: update variable name in GET test `/api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -33,17 +33,15 @@ describe(`GET /api/v1/status`, () => {
   describe(`Default user`, () => {
     test(`Retrieving current system status`, async () => {
       const createdUser = await orchestrator.createUser();
-
       const activatedUser = await orchestrator.activateUser(createdUser);
-
-      const privilegedUserSession = await orchestrator.createSession(
+      const sessionObject = await orchestrator.createSession(
         activatedUser,
       );
 
       const response = await fetch(`${webserver.origin}/api/v1/status`, {
         headers: {
           "Content-Type": "Application/json",
-          Cookie: `session_id=${privilegedUserSession.token}`,
+          Cookie: `session_id=${sessionObject.token}`,
         },
       });
       expect(response.status).toBe(200);
@@ -56,7 +54,6 @@ describe(`GET /api/v1/status`, () => {
       expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
       expect(path.max_connections).toEqual(100);
-
       expect(path.active_connections).toEqual(1);
 
       expect(path.version).toEqual(undefined);
